48ea79a6c82256980b7af0767524cc6f5f2c2c2e
vasavi245 <r.vasavi04@gmail.com>
2020-11-12 15:32:02 +0800

    final commit

--- js/app/ColorsGenerator.js
+++ js/app/ColorsGenerator.js
@@ -1,33 +1,31 @@
-define([
-], function (
-) {
-    // Constants
-    const _COLOR_MIN = 0;    // Minimum possible color value
-    const _COLOR_MAX = 255;  // Maximum possible color value
+define([], function () {
+  // Constants
+  const _COLOR_MIN = 0; // Minimum possible color value
+  const _COLOR_MAX = 255; // Maximum possible color value
 
-    return {
-        /**
-         * Get all the RGB color combinations possible with each color separated
-         * into given step count.
-         *
-         * @param stepCount {integer} number of steps for each color
-         * @returns {Array[]} an array of arrays where each array represents a
-         * discrete RGB value
-         */
-        getDiscreteRGBColors: function (stepCount) {
-            let diff = Math.round((_COLOR_MAX - _COLOR_MIN + 1) / stepCount);   // 8 for stepCount = 32
-            let colors = [];
+  return {
+    /**
+     * Get all the RGB color combinations possible with each color separated
+     * into given step count.
+     *
+     * @param stepCount {integer} number of steps for each color
+     * @returns {Array[]} an array of arrays where each array represents a
+     * discrete RGB value
+     */
+    getDiscreteRGBColors: function (stepCount) {
+      let diff = Math.round((_COLOR_MAX - _COLOR_MIN + 1) / stepCount); // 8 for stepCount = 32
+      let colors = [];
 
-            // Fill possible colors
-            for (let b = _COLOR_MIN; b <= _COLOR_MAX; b = b + diff) {
-                for (let g = _COLOR_MIN; g <= _COLOR_MAX; g = g + diff) {
-                    for (let r = _COLOR_MIN; r <= _COLOR_MAX; r = r + diff) {
-                        colors.push([r, g, b]);
-                    }
-                }                
-            }
-
-            return colors;
+      // Fill possible colors
+      for (let b = _COLOR_MIN; b <= _COLOR_MAX; b = b + diff) {
+        for (let g = _COLOR_MIN; g <= _COLOR_MAX; g = g + diff) {
+          for (let r = _COLOR_MIN; r <= _COLOR_MAX; r = r + diff) {
+            colors.push([r, g, b]);
+          }
         }
-    };
+      }
+
+      return colors;
+    },
+  };
 });
--- js/app/ImageGenerator.js
+++ js/app/ImageGenerator.js
@@ -1,35 +1,35 @@
-define([
-    'app/ColorsGenerator',
-    'app/DOMManipulator'
-], function (
-    ColorsGenerator,
-    DOMManipulator
+define(["app/ColorsGenerator", "app/DOMManipulator"], function (
+  ColorsGenerator,
+  DOMManipulator
 ) {
-    return {
-        run: function () {
-             $('.linear').append(this._createLinearImage());
-     
-           
-        },
+  return {
+    run: function () {
+      $(".linear").append(this._createLinearImage());
+    },
 
-        _createLinearImage: function () {
-            let nRows = 256, nColumns = 128;
-            let discreteColors = ColorsGenerator.getDiscreteRGBColors(32);
-            let imgWrapper = DOMManipulator.createImageDiv('linear-wrap');
-            let nColors = discreteColors.length - 1;
+    _createLinearImage: function () {
+      let nRows = 256,
+        nColumns = 128;
+      let discreteColors = ColorsGenerator.getDiscreteRGBColors(32);
+      let imgWrapper = DOMManipulator.createImageDiv("linear-wrap");
+      let nColors = discreteColors.length - 1;
 
-            for (let column = 0; column < nColumns; column++) {
-                let rowContainer = DOMManipulator.createRowContainerDiv();
-                imgWrapper.append(rowContainer);
-                for (let row = 0; row < nRows; row++) {
-                    let color = discreteColors[nColors];
-                    let pixelDiv = DOMManipulator.createPixelDiv(color[0], color[1], color[2]);
-                    rowContainer.append(pixelDiv);
-                    nColors --;
-                }
-            }
-
-            return imgWrapper;
+      for (let column = 0; column < nColumns; column++) {
+        let rowContainer = DOMManipulator.createRowContainerDiv();
+        imgWrapper.append(rowContainer);
+        for (let row = 0; row < nRows; row++) {
+          let color = discreteColors[nColors];
+          let pixelDiv = DOMManipulator.createPixelDiv(
+            color[0],
+            color[1],
+            color[2]
+          );
+          rowContainer.append(pixelDiv);
+          nColors--;
         }
-    };
+      }
+
+      return imgWrapper;
+    },
+  };
 });
